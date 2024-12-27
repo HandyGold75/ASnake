f4e2016e12bf5b29aba5f4e691c3a9bfa66c2a3d
HandyGold75 <IZO@HandyGold75.com>
2024-12-27 12:54:56 +0100

    27-Dec-24

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 update(){
-    go get go@latest
-    go mod tidy || { echo -e "\033[31mFailed: $1.*\033[0m" ; return 1; }
+    go get go@latest || { echo -e "\033[31mFailed: $1.*\033[0m" ; return 1; }
     go get -u || { echo -e "\033[31mFailed: $1.*\033[0m" ; return 1; }
+    go mod tidy || { echo -e "\033[31mFailed: $1.*\033[0m" ; return 1; }
+    for indirect in $(tail +3 go.mod | grep "// indirect" | awk '{if ($1 =="require") print $2; else print $1;}'); do go get -u "${indirect}"; done
+    go get -u || { echo -e "\033[31mFailed: $1.*\033[0m" ; return 1; }
+    go mod tidy || { echo -e "\033[31mFailed: $1.*\033[0m" ; return 1; }
 }
+
 
 build(){
     ( env GOOS=linux GOARCH=amd64 go build -o "$1.bin" . && echo -e "\033[32mBuild: $1.bin\033[0m" ) || { echo -e "\033[31mFailed: $1.bin\033[0m" ; return 1; }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module ASnake
 go 1.23.4
 
 require (
-	github.com/HandyGold75/GOLib/logger v0.0.0-20241227095000-199895e57b21
+	github.com/HandyGold75/GOLib/logger v0.0.0-20241227105215-5368016948f0
 	golang.org/x/term v0.27.0
 )
 
